521e154efcf8f28e38179bac6aa6dbf4b6d15b88
Andrew Shlegel <a.shlegel@outlook.com>
2023-03-07 14:12:35 -0300

    Task029 added

---
+++ Task029/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. 
+// Данные вводятся с консоли пользователем
+
+int[] CreateArray()
+{
+    int[] array = new int[8];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите {i + 1}-ый элемент массива: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+Console.WriteLine(string.Join(", ", CreateArray()));
