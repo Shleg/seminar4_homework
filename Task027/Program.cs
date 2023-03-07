3602ec198b362b0114111d0d6e75768621498c17
Andrew Shlegel <a.shlegel@outlook.com>
2023-03-07 14:12:25 -0300

    Task027 added

---
+++ Task027/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int InputNumber(string msgToUser)
+{
+    Console.Write(msgToUser);
+    int result = 0;
+    if (int.TryParse(Console.ReadLine(), out result))
+    {
+        return result;
+    }
+    else
+    {
+        throw new Exception("Waiting a integer number! Restart the program!");
+    }
+}
+
+int DigitSum(int num)
+{
+    if (num == 0) return 0;
+    return num % 10 + DigitSum(num / 10);
+}
+
+int number = InputNumber("Enter an integer number: ");
+Console.WriteLine($"{number} -> {DigitSum(number)}");
